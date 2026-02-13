6133f579c7bb6ef91e67f08ef1ffef11c7ea3a25
Ki-Will <irakozep03@gmail.com>
2026-02-13 14:53:40 +0200

    Add start_jdbc.bat

---
+++ start_jdbc.bat
@@ -0,0 +1,4 @@
+@echo off
+set CATALINA_HOME=D:\apache-tomcat-9.0.113-windows-x64\apache-tomcat-9.0.113
+set SCHOOLMGMT_PERSISTENCE=JDBC
+D:\apache-tomcat-9.0.113-windows-x64\apache-tomcat-9.0.113\bin\startup.bat
